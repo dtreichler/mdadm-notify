9cbbcf3dd3dbdfeb7a717472e2f895e496a9f516
Derrick Treichler <dtreichl@gmail.com>
2012-09-13 08:56:09 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+# Filename: setup.py
+# Created: Thu Sep 13 07:46:06 2012
+# Created By: dtreichler
+# Modified: Thu Sep 13 08:42:35 2012
+# Modified By: dtreichler
+
+from setuptools import setup
+
+setup(
+    name = "mdadm-notify",
+    version = "0.1",
+    author = "Derrick Treichler",
+    description = ("A simple monitoring script for mdadm that emails outside ",
+                   "addresses"),
+    license = "BSD",
+    scripts = [
+        'mdadm-notify'
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: System :: Monitoring",
+    ],
+)
+
+
